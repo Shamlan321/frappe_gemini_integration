fe9a7bba31707505cea76066c9e75b209d3fd767
Shamlan Arshad <shamlan.arshad321@gmail.com>
2025-08-13 19:12:14 +0900

    Update __init__.py

--- frappe_gemini_integration/__init__.py
+++ frappe_gemini_integration/__init__.py
@@ -1,8 +1,1 @@
-from . import __version__ as app_version
-
-app_name = "frappe_gemini_integration"
-app_version = "0.0.1"
-app_publisher = "Manav Mandli"
-app_description = "Gemini Integration in Frappe Framework"
-app_email = "manavmandli2990@gmail.com"
-app_license = "mit"
+__version__ = "0.0.1"
